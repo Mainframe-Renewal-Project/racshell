27d1cec0b0dbfadedbba74ba9bd62333b63a37f1
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-12-10 00:30:57 +0100

    Initial work

--- racshell/listuser.cpp
+++ racshell/listuser.cpp
@@ -22,6 +22,10 @@ int main(int argc, char *argv[]) {
 
     if (input.length() > 8) {
         std::cout << std::string("\u001b[31mRACSHELL Error\x1b[0m: Invalid input, must be a valid RACF userid \n");
+    } else {
+        std::cout << std::string("--------------------------------------------------------------------");
+        std::cout << std::string("Listuser: ") << input;
+        std::cout << std::string("--------------------------------------------------------------------");
     }
 
     return 0;
