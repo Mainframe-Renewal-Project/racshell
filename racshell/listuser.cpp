b27396461edf1d49dee7dd6f334c08172a6c659f
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-12-10 00:43:00 +0100

    Initial work

--- racshell/listuser.cpp
+++ racshell/listuser.cpp
@@ -6,8 +6,16 @@
 int main(int argc, char *argv[]) {
     argparse::ArgumentParser program("listuser");
 
+    // Main parameter, the user to list
     program.add_argument("user")
     .help("RACF user to list");
+
+    // Optional parameters for data not displayed by default
+    program.add_argument("-g", "--groups")
+    .help("List connected RACF groups");
+
+    program.add_argument("-t", "--tso")
+    .help("List TSO segment");
 
     try {
         program.parse_args(argc, argv);
@@ -20,6 +28,7 @@ int main(int argc, char *argv[]) {
 
     std::string input = program.get<std::string>("user");
 
+    // Check if the input is too long to be a valid user
     if (input.length() > 8) {
         std::cout << std::string("\u001b[31mRACSHELL Error\x1b[0m: Invalid input, must be a valid RACF userid \n");
     } else {
